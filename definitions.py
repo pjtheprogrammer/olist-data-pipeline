1745eb169c9f4f21fd7dd0b4af86ca1cdfda53ce
pjtheprogrammer <www.paulefeoku@gmail.com>
2026-06-11 11:25:18 -0700

    chore: added schedule to run automatically in dagster cloud

--- definitions.py
+++ definitions.py
@@ -1,5 +1,5 @@
 import os
-from dagster import Definitions
+from dagster import Definitions, ScheduleDefinition, define_asset_job
 from dagster_dbt import DbtCliResource, dbt_assets
 from pathlib import Path
 
@@ -9,6 +9,13 @@ dbt_project_dir = Path(__file__).parent.absolute()
 @dbt_assets(manifest=dbt_project_dir / "target/manifest.json")
 def olist_dbt_assets(context, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+olist_dbt_job = define_asset_job(name="olist_dbt_job", selection="*")
+
+olist_dbt_schedule = ScheduleDefinition(
+    job=olist_dbt_job,
+    cron_schedule="0 0 * * *", 
+)
 
 defs = Definitions(
     assets=[olist_dbt_assets],
