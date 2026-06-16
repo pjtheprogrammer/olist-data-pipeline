88d9d5cbcf7b291122748ec07eb29a207ba5f39f
pjtheprogrammer <www.paulefeoku@gmail.com>
2026-06-15 23:13:21 -0700

    fix: changed calculation for total_order_value

--- models/marts/fct_revenue_per_order.sql
+++ models/marts/fct_revenue_per_order.sql
@@ -1,8 +1,9 @@
 select 
     o.order_id,
     count(oi.order_item_id) as no_of_items,
-    sum(oi.price) as price,
-    sum(oi.freight_value) as freight_cost
+    sum(oi.price) as total_price,
+    sum(oi.freight_value) as freight_cost,
+    total_price + freight_cost as total_order_value
 from {{ref('stg_orders')}} o
 join {{ref('stg_order_items')}} oi on o.order_id = oi.order_id
 group by o.order_id
